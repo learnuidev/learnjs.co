06b1aa5b2bccc1cc8c65658a38030cbae80aabc6
learnuidev <learnuidev@gmail.com>
2025-02-24 12:05:18 -0500

    added blueprint

---
+++ app/c/[course-id]/components/the-blueprint.tsx
@@ -0,0 +1,54 @@
+export const TheBlueprint = () => {
+  return (
+    <section
+      id="the-blueprint"
+      className="mt-32 py-16 px-12 mb-32 bg-orange-50 rounded-xl dark:bg-[rgb(31,32,33)]"
+    >
+      <h3 className="text-5xl text-center font-bold">The Blueprint</h3>
+
+      <p className="text-center font-mono mt-4 text-xl">
+        <span>
+          At learnjs.co our Blueprint is simple: Teaching Practical, Useful
+          concepts based on Real World Examples.
+        </span>
+      </p>
+
+      <p className="font-mono mt-12 text-xl">
+        <span>
+          Before we start teaching any concept, we always start this fundamental
+          question: WHY? Why do we need to learn it? Asking allows us to
+          establish the right context. As a result you will be better prepping
+          and motivated you for the actual learning material. If we dont know
+          why we are learning, then its very easy to forget or even appreciate
+          why we are learning it.
+        </span>
+      </p>
+      <p className="font-mono mt-12 text-xl">
+        <span>
+          Most JavaScript courses are long, boring, and frankly useless. If I
+          had a dime for every time I saw examples such as x(), foo(), I would
+          be richer than Ali Abdaal. All the examples use code from Real World
+          Code Bases, which can be copy pasted right into your app. Which means,
+          no more useless foo, bar and baz and shipping more useful code. More
+          over all of our lessons are less than 5 minutes. This is by design.
+          Study shows that the longer the content, the shorter the attention
+          span is likely to be, as people tend to have a harder time focusing on
+          information for extended periods.
+        </span>
+      </p>
+
+      <p className="font-mono mt-12 text-xl">
+        <span>
+          Each lesson is self contained, meaning it doesnt depend on any others.
+        </span>
+      </p>
+      <p className="font-mono mt-12 text-xl">
+        <span>
+          Whether you want to take the linear approach, or simply want to learn
+          about specific concept, its up to YOU. You are incharge (and you
+          should be) of your learning.
+        </span>
+      </p>
+    </section>
+  );
+};
--- app/c/[course-id]/page.tsx
+++ app/c/[course-id]/page.tsx
@@ -6,6 +6,7 @@ import { useGetCourse } from "./hooks/use-get-course";
 import { useGetCourseParams } from "./hooks/use-get-course-params";
 import { PageTitle } from "@/components/page-title";
 import { CourseContent } from "./components/course-content";
+import { TheBlueprint } from "./components/the-blueprint";
 
 export default function Page() {
   const { courseId } = useGetCourseParams();
@@ -19,7 +20,7 @@ export default function Page() {
     <main>
       <PageTitle title={course.title} description={course.description} />
       <CourseBanner course={course} />
-
+      <TheBlueprint />
       <CourseContent course={course} />
     </main>
   );
