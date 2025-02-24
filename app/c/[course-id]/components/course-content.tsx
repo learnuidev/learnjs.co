a9f29e7cadc4d5f73afdb5867e80b857e28cfb73
learnuidev <learnuidev@gmail.com>
2025-02-24 11:21:42 -0500

    added course details

--- app/c/[course-id]/components/course-content.tsx
+++ app/c/[course-id]/components/course-content.tsx
@@ -1,12 +1,18 @@
 import { useTranslation } from "@/lib/i18n-next/use-translation";
 import { Course } from "@/modules/courses/course.type";
+import { useGetCourseDetails } from "../hooks/use-get-course-details";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export const CourseContent = ({ course }: { course: Course }) => {
   const { t } = useTranslation(["courseDetails"]);
+
+  const courseContent = useGetCourseDetails();
   return (
     <section
       id="course-content"
-      className="h-screen mt-32 py-16 px-12 bg-orange-50 rounded-xl dark:bg-[rgb(31,32,33)]"
+      className="mt-32 py-16 px-12 mb-32 bg-orange-50 rounded-xl dark:bg-[rgb(31,32,33)]"
     >
       <h3 className="text-5xl text-center font-bold">
         {t("courseDetails:course.content.title")}
@@ -16,6 +22,36 @@ export const CourseContent = ({ course }: { course: Course }) => {
         <span>{t("courseDetails:course.content.description")} </span>
         <span className="font-bold">{course.title}</span>
       </p>
+
+      <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-8 mt-12">
+        {courseContent.map((course) => {
+          return (
+            <Card
+              key={course.id}
+              className={cn(
+                "rounded-2xl p-8 hover:scale-105 transition hover:bg-yellow-400",
+                "dark:bg-[rgb(11,12,13)] bg-white dark:text-white"
+              )}
+            >
+              <Link href={`/c/${course.id}`} className="inline-block h-auto">
+                <CardTitle>{course.title}</CardTitle>
+
+                <CardDescription className="dark:text-gray-400 font-mono">
+                  {course.description}
+                </CardDescription>
+              </Link>
+
+              {/* <Link href={`/c/${course.id}`}>
+                <div className="mt-4 flex justify-between w-full">
+                  <span className="underline"> {t("common:learn.more")} </span>
+
+                  {course.type === "free" && course.typeI18n}
+                </div>
+              </Link> */}
+            </Card>
+          );
+        })}
+      </div>
     </section>
   );
 };
--- app/c/[course-id]/hooks/use-get-course-details.ts
+++ app/c/[course-id]/hooks/use-get-course-details.ts
@@ -1,9 +1,63 @@
-export const useGetCourseDetails = (id: string) => {
-  console.log(id);
+import { useGetCourseParams } from "./use-get-course-params";
+
+export const useGetCourseDetails = () => {
+  const { courseId } = useGetCourseParams();
+
+  console.log(courseId);
   return [
     {
+      id: "execution-contexts",
+      title: "01. Execution Contexts",
+      description:
+        "Understand what execution contexts are, how they are created, and their role in JavaScript code execution.",
+    },
+    {
+      id: "call-stack",
+      title: "02. Call Stack",
+      description:
+        "Explore the call stack, how it manages function calls, and its importance in tracking execution flow.",
+    },
+    {
+      id: "hoisting",
+      title: "03. Hoisting",
+      description:
+        "Learn about hoisting, how variable and function declarations are moved to the top of their scope during compilation.",
+    },
+    {
+      id: "tdz",
+      title: "04. Temporal Dead Zone",
+      description:
+        "Discover the Temporal Dead Zone (TDZ), why it occurs, and how it affects variable access in JavaScript.",
+    },
+    {
+      id: "prototype",
+      title: "05. Prototype",
+      description:
+        "Dive into JavaScript's prototype system, how inheritance works, and how objects share properties and methods.",
+    },
+    {
+      id: "this",
+      title: "06. this",
+      description:
+        "Master the `this` keyword, its binding rules, and how its value is determined in different contexts.",
+    },
+    {
+      id: "scope",
+      title: "07. Scope & Scope Chain",
+      description:
+        "Learn about scope, how it determines variable visibility, and how the scope chain resolves variable references.",
+    },
+    {
       id: "closures",
-      title: "Closures",
+      title: "08. Closures",
+      description:
+        "Understand closures, how they capture and retain access to variables, and their practical applications in JavaScript.",
+    },
+    {
+      id: "async-programming",
+      title: "09. Async Programming",
+      description:
+        "Master asynchronous programming in JavaScript. Explore concepts like callbacks, Promises, and async/await. Learn to handle concurrent operations, manage API calls, and write efficient non-blocking code for improved application performance and responsiveness.",
     },
   ];
 };
