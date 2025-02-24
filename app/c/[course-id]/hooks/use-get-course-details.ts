88f8de03fd4bdaa911bf863c2bef147f304312e2
learnuidev <learnuidev@gmail.com>
2025-02-24 11:26:23 -0500

    wip

--- app/c/[course-id]/hooks/use-get-course-details.ts
+++ app/c/[course-id]/hooks/use-get-course-details.ts
@@ -19,45 +19,45 @@ export const useGetCourseDetails = () => {
     },
     {
       id: "hoisting",
-      title: "03. Hoisting",
+      title: "03. Hoisting & Temporal Dead Zone",
       description:
-        "Learn about hoisting, how variable and function declarations are moved to the top of their scope during compilation.",
-    },
-    {
-      id: "tdz",
-      title: "04. Temporal Dead Zone",
-      description:
-        "Discover the Temporal Dead Zone (TDZ), why it occurs, and how it affects variable access in JavaScript.",
+        "Learn about hoisting, how variable and function declarations are moved to the top of their scope during compilation, and understand the concept of the Temporal Dead Zone in JavaScript.",
     },
     {
       id: "prototype",
-      title: "05. Prototype",
+      title: "04. Prototype",
       description:
         "Dive into JavaScript's prototype system, how inheritance works, and how objects share properties and methods.",
     },
     {
       id: "this",
-      title: "06. this",
+      title: "05. this",
       description:
         "Master the `this` keyword, its binding rules, and how its value is determined in different contexts.",
     },
     {
       id: "scope",
-      title: "07. Scope & Scope Chain",
+      title: "06. Scope & Scope Chain",
       description:
         "Learn about scope, how it determines variable visibility, and how the scope chain resolves variable references.",
     },
     {
       id: "closures",
-      title: "08. Closures",
+      title: "07. Closures",
       description:
         "Understand closures, how they capture and retain access to variables, and their practical applications in JavaScript.",
     },
     {
       id: "async-programming",
-      title: "09. Async Programming",
+      title: "08. Async Programming",
       description:
         "Master asynchronous programming in JavaScript. Explore concepts like callbacks, Promises, and async/await. Learn to handle concurrent operations, manage API calls, and write efficient non-blocking code for improved application performance and responsiveness.",
+    },
+    {
+      id: "composition",
+      title: "09. Composition",
+      description:
+        "Explore the powerful concept of composition in JavaScript. Learn how to build complex functionality by combining smaller, reusable functions. Understand the benefits of composition over inheritance, discover techniques for creating flexible and maintainable code, and master the art of composing functions for more modular and scalable applications.",
     },
   ];
 };
