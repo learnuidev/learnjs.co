a90ce629d9ab7e052cc5eeb7cfb29700fb7d08f0
learnuidev <learnuidev@gmail.com>
2025-02-24 12:23:48 -0500

    added blueprint

--- app/c/[course-id]/components/the-blueprint.tsx
+++ app/c/[course-id]/components/the-blueprint.tsx
@@ -1,54 +1,91 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export const TheBlueprint = () => {
   return (
     <section
       id="the-blueprint"
-      className="mt-32 py-16 px-12 mb-32 bg-orange-50 rounded-xl dark:bg-[rgb(31,32,33)]"
+      className="mt-32 py-16 px-12 mb-32 bg-yellow-50 rounded-xl dark:bg-[rgb(31,32,33)]"
     >
       <h3 className="text-5xl text-center font-bold">The Blueprint</h3>
 
-      <p className="text-center font-mono mt-4 text-xl">
-        <span>
-          At learnjs.co our Blueprint is simple: Teaching Practical, Useful
-          concepts based on Real World Examples.
-        </span>
-      </p>
+      <div className="mt-12 flex flex-col lg:flex-row gap-8">
+        <p className="font-mono text-xl">
+          <span>
+            At learnjs.co our Blueprint is simple: Teaching Practical, Copy
+            Pastable and Useful concepts based on Real World Examples.
+          </span>
+        </p>
 
-      <p className="font-mono mt-12 text-xl">
-        <span>
-          Before we start teaching any concept, we always start this fundamental
-          question: WHY? Why do we need to learn it? Asking allows us to
-          establish the right context. As a result you will be better prepping
-          and motivated you for the actual learning material. If we dont know
-          why we are learning, then its very easy to forget or even appreciate
-          why we are learning it.
-        </span>
-      </p>
-      <p className="font-mono mt-12 text-xl">
-        <span>
-          Most JavaScript courses are long, boring, and frankly useless. If I
-          had a dime for every time I saw examples such as x(), foo(), I would
-          be richer than Ali Abdaal. All the examples use code from Real World
-          Code Bases, which can be copy pasted right into your app. Which means,
-          no more useless foo, bar and baz and shipping more useful code. More
-          over all of our lessons are less than 5 minutes. This is by design.
-          Study shows that the longer the content, the shorter the attention
-          span is likely to be, as people tend to have a harder time focusing on
-          information for extended periods.
-        </span>
-      </p>
+        <Accordion
+          className="w-full lg:w-[unset] space-y-8"
+          type="single"
+          collapsible
+        >
+          <AccordionItem className="lg:w-[500px] max-w-full" value="why">
+            <AccordionTrigger>Start with WHY?</AccordionTrigger>
+            <AccordionContent>
+              <p>
+                Before we start teaching any concept, we always start this
+                fundamental question: WHY? Why do we need to learn it? Asking
+                allows us to establish the right context.
+              </p>
+              <p>
+                As a result you will be better prepping and motivated you for
+                the actual learning material. If we dont know why we are
+                learning, then its very easy to forget or even appreciate why we
+                are learning it.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem
+            className="lg:w-[500px] max-w-full"
+            value="short-practical-real-world"
+          >
+            <AccordionTrigger>
+              Short, Practical based on Real World Codebase
+            </AccordionTrigger>
+            <AccordionContent>
+              <p>
+                Before we start teaching any concept, we always start this
+                fundamental question: WHY? Why do we need to learn it? Asking
+                allows us to establish the right context.
+              </p>
 
-      <p className="font-mono mt-12 text-xl">
-        <span>
-          Each lesson is self contained, meaning it doesnt depend on any others.
-        </span>
-      </p>
-      <p className="font-mono mt-12 text-xl">
-        <span>
-          Whether you want to take the linear approach, or simply want to learn
-          about specific concept, its up to YOU. You are incharge (and you
-          should be) of your learning.
-        </span>
-      </p>
+              <p>
+                As a result you will be better prepping and motivated you for
+                the actual learning material.
+              </p>
+              <p>
+                If we dont know why we are learning, then its very easy to
+                forget or even appreciate why we are learning it.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem
+            className="lg:w-[500px] max-w-full"
+            value="flexible-learning-path"
+          >
+            <AccordionTrigger>Flexible Learning Path</AccordionTrigger>
+            <AccordionContent>
+              <p>
+                Each lesson is self contained, meaning it doesnt depend on any
+                others.
+              </p>
+              <p>
+                Whether you want to take the linear approach, or simply want to
+                learn about specific concept, its up to YOU.
+              </p>
+              <p>You are incharge (and you should be) of your learning.</p>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </section>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,38 +11,59 @@ export default {
     "./modules/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        main: "var(--main)",
-        overlay: "var(--overlay)",
-        bg: "var(--bg)",
-        bw: "var(--bw)",
-        blank: "var(--blank)",
-        text: "var(--text)",
-        mtext: "var(--mtext)",
-        border: "var(--border)",
-        ring: "var(--ring)",
-        ringOffset: "var(--ring-offset)",
-
-        secondaryBlack: "#212121",
-      },
-      borderRadius: {
-        base: "5px",
-      },
-      boxShadow: {
-        shadow: "var(--shadow)",
-      },
-      translate: {
-        boxShadowX: "4px",
-        boxShadowY: "4px",
-        reverseBoxShadowX: "-4px",
-        reverseBoxShadowY: "-4px",
-      },
-      fontWeight: {
-        base: "500",
-        heading: "700",
-      },
-    },
+  	extend: {
+  		colors: {
+  			main: 'var(--main)',
+  			overlay: 'var(--overlay)',
+  			bg: 'var(--bg)',
+  			bw: 'var(--bw)',
+  			blank: 'var(--blank)',
+  			text: 'var(--text)',
+  			mtext: 'var(--mtext)',
+  			border: 'var(--border)',
+  			ring: 'var(--ring)',
+  			ringOffset: 'var(--ring-offset)',
+  			secondaryBlack: '#212121'
+  		},
+  		borderRadius: {
+  			base: '5px'
+  		},
+  		boxShadow: {
+  			shadow: 'var(--shadow)'
+  		},
+  		translate: {
+  			boxShadowX: '4px',
+  			boxShadowY: '4px',
+  			reverseBoxShadowX: '-4px',
+  			reverseBoxShadowY: '-4px'
+  		},
+  		fontWeight: {
+  			base: '500',
+  			heading: '700'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
 
   plugins: [require("tailwindcss-animate")],
